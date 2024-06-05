47a063afa439e91033694ed0c034c2be1d38ac92
ValentinBaldini <valentinbaldini28@gmail.com>
2024-06-04 21:38:52 -0300

    creacion de repo

---
+++ src/main/java/com/example/JPAElBuenSabor/JpaElBuenSaborApplication.java
@@ -0,0 +1,13 @@
+package com.example.JPAElBuenSabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaElBuenSaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaElBuenSaborApplication.class, args);
+	}
+
+}
